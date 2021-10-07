1a50580508d2fc06686b85ebaf03f68b0397d04e
2021093054 <effect2110@gmail.com>
2021-10-07 22:37:05 +0900

    Graph Theory Update
    Bellman Ford

---
+++ Code/Graph Theory/Shortest Path/Bellman-Ford.cpp
@@ -0,0 +1,35 @@
+//have to include these headers
+#include <vector>
+#include <utility>
+#define INF 987654321
+
+using namespace std;
+
+int N;
+vector<int> dist(N, INF);
+
+//O(VE) bellman ford algorithm
+void bellman_ford(vector<vector<pair<int, int>>> adj, int start) {
+    dist[start] = 0;
+
+    //update dist
+    for(int i = 1; i <= (N - 1); ++i) { //after repeat N - 1 times, it completes.
+        for(int j = 1; j <= N; ++j) {
+            for(auto& edge : adj[j]) { //edge.first = destination, edge.second = distance
+                dist[edge.first] = min(dist[j] + edge.second, dist[edge.first]);
+            }
+        }
+    }
+
+    //check negative cycle
+    for(int i = 1; i <= N; ++i) {
+        for(auto edge : adj[i]) {
+            if(dist[edge.first] > dist[i] + edge.second) { //if dist changes, it means that it has negative cycle
+                //cout << "YES\n";
+                return;
+            }
+        }
+    }
+
+    //cout << "NO\n";
+}
